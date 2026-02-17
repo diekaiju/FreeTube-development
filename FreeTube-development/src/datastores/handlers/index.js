2a11a14f38ed648d5433f7d6b9140118edd03a51
siddiq2772 <siddiqmohd2772@gmail.com>
2026-02-17 11:43:44 +0530

    chore: Remove a large portion of renderer components, views, helpers, assets, localization files, and GitHub workflows.

--- FreeTube-development/src/datastores/handlers/index.js
+++
@@ -1,8 +0,0 @@
-export {
-  settings as DBSettingHandlers,
-  history as DBHistoryHandlers,
-  profiles as DBProfileHandlers,
-  playlists as DBPlaylistHandlers,
-  searchHistory as DBSearchHistoryHandlers,
-  subscriptionCache as DBSubscriptionCacheHandlers,
-} from 'DB_HANDLERS_ELECTRON_RENDERER_OR_WEB'
--- FreeTube-development/src/renderer/components/ft-flex-box/ft-flex-box.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="ft-flex-box">
-    <slot class="flex-container" />
-  </div>
-</template>
-
-<script src="./ft-flex-box.js" />
-<style scoped src="./ft-flex-box.css" />
